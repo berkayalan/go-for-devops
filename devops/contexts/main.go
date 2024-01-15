67d1bb93347f07ad12cab5e1bbd2f8fa6241918a
berkayalan <berkayalan.mail@gmail.com>
2024-01-15 08:20:26 +0300

    contexts first example added

--- devops/contexts/main.go
+++ devops/contexts/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func Greetings(ctx context.Context) {
+
+	fmt.Printf("Welcome %v ,\nWe learn golang here!!!", ctx.Value("name"))
+
+	/*
+
+		We can return the name for future usage
+
+		return ctx.Value("name")
+	*/
+}
+
+func main() {
+
+	ctx := context.Background()
+
+	ctx = context.WithValue(ctx, "name", "Berkay") // "myKey", "myValue"
+
+	Greetings(ctx)
+}
